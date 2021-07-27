ae9c3858468224cc4f6d1467038c700162321f48
Art-Orl <orlyuta95@mail.ru>
2021-07-27 18:18:27 +0300

    feat: add form

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <div className="form__cntainer">
+      <form>
+        <input type="text" />
+        <input type="date"/>
+        <select />
+      </form>
+    </div>
+  );
+}
